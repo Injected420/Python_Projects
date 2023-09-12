92f76587569fb3b90f6d882b071843bb74ef9c8d
Chris <slybandit247@gmail.com>
2023-09-12 11:38:34 -0400

    Update portscanner.py
    Added Start time, changed color output

--- portscanner.py
+++ portscanner.py
@@ -1,7 +1,10 @@
 import socket
 import threading
 import IPy as ipy
-import requests, sys
+import requests
+import sys
+import datetime
+
 
 class Color:
     RESET = "\033[0m"
@@ -14,14 +17,15 @@ class Color:
     WHITE = "\033[37m"
     BOLD = "\033[1m"
     UNDERLINE = "\033[4m"
-    
 
+
+START = datetime.datetime.now()
 
 
 def checkIP(ip):
     try:
         ipy.IP(ip)
-        return(ip)
+        return (ip)
     except ValueError:
         return socket.gethostbyname(ip)
 
@@ -31,25 +35,31 @@ def port_scan(ipaddress, port):
         sock = socket.socket()
         sock.settimeout(0.10)
         sock.connect((ipaddress, port))
-        print(f'[{Color.GREEN}+{Color.RESET}] Port: {Color.BOLD}{port}{Color.RESET} is Open.')
-
-    except :
-        print(f'[{Color.RED}-{Color.RESET}] Port: {Color.BOLD}{port}{Color.RESET} is Closed')
-
-
+        print(
+            f'{Color.GREEN}[+]{Color.RESET} Port: {Color.BOLD}{port}{Color.RESET} is Open.')
+    except KeyboardInterrupt:
+        print(f"\r\n{Color.RED}[!]{Color.RESET} Program exited by user")
+        sys.exit(1)
+    except:
+        print(
+            f'{Color.YELLOW}[-]{Color.RESET} Port: {Color.BOLD}{port}{Color.RESET} is Closed')
 
 
 def do_scan():
     try:
         IPADDRESS = input(f"{Color.BLUE}${Color.RESET} Enter Target to Scan: ")
-        port = int(input(f"{Color.BLUE}${Color.RESET} Enter the last port number you want to scan: "))
-    
+        port = int(input(
+            f"{Color.BLUE}${Color.RESET} Enter the last port number you want to scan: "))
+        print(f"{Color.BOLD}[*] Host{Color.RESET}:",
+              socket.gethostbyname(IPADDRESS), "\r\nScan started at: \r\n", START)
+
         converted_ip = checkIP(IPADDRESS)
-    
+
         for i in range(1, port + 1):
             port_scan(converted_ip, i)
     except KeyboardInterrupt:
         print(f"\n[{Color.MAGENTA}*{Color.RESET}] Scanning Ended by user {Color.BOLD}{Color.CYAN}(Ctrl+c){Color.RESET}. Exiting...\n")
         sys.exit()
+
 
 do_scan()
